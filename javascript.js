b8be5a44b4201aade89a762a6ba35084a969487c
Jun Kai Lim <junkaiii@gmail.com>
2016-07-11 17:18:27 +0800

    added basic 2 player

--- javascript.js
+++ javascript.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
 
   var listOfColor = ['red', 'green', 'blue']; //list of colors must be similar to class name
 
-  var boxList = [];
+
+  var oneBoxList = [];
+  var twoBoxList = [];
+
 
 
   var randomiseColor = function(min, max) { // randomiser function
@@ -14,13 +17,16 @@ $(document).ready(function() {
 
   var makeDivs = function() { // randomises each div created and fills up boxList up to the numOfBoxes defined
     for (var i = 0; i < numOfBoxes; i++) {
-      boxList[i] = listOfColor[randomiseColor(0, 2)];
-      $('#container').append('<div class="' + boxList[i] + ' box"></div>');
+      oneBoxList[i] = listOfColor[randomiseColor(0, 2)];
+      $('#container').append('<div class="' + oneBoxList[i] + ' box"></div>');
+      twoBoxList[i] = listOfColor[randomiseColor(0, 2)];
+      $('#container2').append('<div class="' + twoBoxList[i] + ' boxtwo"></div>');
     }
   };
 
   makeDivs();
 
+//player 1 --------
 
   // if box is red and if a is clicked run this function
   $(document).keydown(function(a) {
@@ -48,7 +54,7 @@ $(document).ready(function() {
     }
   });
 
-  // if box is green and if s is clicked run this function
+  // if box is green and if d is clicked run this function
   $(document).keydown(function(d) {
     if ($(".box").first().attr('class') === 'green box' && d.keyCode == 68) {
       console.log('clicked');
@@ -59,8 +65,56 @@ $(document).ready(function() {
         $(this).remove();
       });
     }
+
+
   });
 
+  //player 1 --------
+
+  //player 2 --------
+
+    // if box is red and if j is clicked run this function
+    $(document).keydown(function(j) {
+      if ($(".boxtwo").first().attr('class') === 'red boxtwo' && j.keyCode == 74) {
+        console.log('clicked');
+        $(".boxtwo").first().css("position", "absolute").css("left", "300px").removeClass("boxtwo").animate({
+          marginLeft: "+=100",
+          opacity: 0
+        }, 200, function() {
+          $(this).remove();
+        });
+      }
+    });
+
+    // if box is blue and if k is clicked run this function
+    $(document).keydown(function(k) {
+      if ($(".boxtwo").first().attr('class') === 'blue boxtwo' && k.keyCode == 75) {
+        console.log('clicked');
+        $(".boxtwo").first().css("position", "absolute").css("left", "300px").removeClass("boxtwo").animate({
+          marginLeft: "+=100",
+          opacity: 0
+        }, 200, function() {
+          $(this).remove();
+        });
+      }
+    });
+
+    // if box is green and if d is clicked run this function
+    $(document).keydown(function(l) {
+      if ($(".boxtwo").first().attr('class') === 'green boxtwo' && l.keyCode == 76) {
+        console.log('clicked');
+        $(".boxtwo").first().css("position", "absolute").css("left", "300px").removeClass("boxtwo").animate({
+          marginLeft: "+=100",
+          opacity: 0
+        }, 200, function() {
+          $(this).remove();
+        });
+      }
+
+
+    });
+
+    //player 2 --------
 
 
 });
